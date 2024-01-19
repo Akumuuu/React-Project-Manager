017d941403f89f41d23c28086c870032f9020df2
Akumuuu <88854566+Akumuuu@users.noreply.github.com>
2024-01-19 20:38:42 +0100

    Add files via upload

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,42 @@
+import Button from "./Button";
+
+export default function ProjectsSidebar({
+  onStartAddProject,
+  projects,
+  onSelectProject,
+  selectedProjectId,
+}) {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        Your Projects
+      </h2>
+      <div>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
+      </div>
+      <ul>
+        {projects.map((projects) => {
+          let cssClasses =
+            "w-full text-left px-2 py-1 rounded-sm my-1  hover:text-stone-200 hover:bg-stone-800";
+
+          if (projects.id === selectedProjectId) {
+            cssClasses += " bg-stone-800 text-stone-200 ";
+          } else {
+            cssClasses += " text-stone-400";
+          }
+
+          return (
+            <li key={projects.id}>
+              <button
+                className={cssClasses}
+                onClick={() => onSelectProject(projects.id)}
+              >
+                {projects.title}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+}
